0db3fe6b6dfda223391d0a96419548bcf3605501
takeshi.hirosue <mirrored1976@gmail.com>
2022-02-13 21:16:15 +0900

    feat: use ui-library compornents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavBar, HeroLayout4, CardC, MarketingFooter  } from './ui-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar width={'100%'} />
+      <HeroLayout4 width={'100%'} />
+      <div className="my-8">
+        Contents
+      </div>
+      <MarketingFooter width={'100%'} />
+    </>
   );
 }
 
